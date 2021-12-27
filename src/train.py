bca3619ab1db5604776b43d3c6934bd10e4b2086
Michele Bortone <33963215+mibo8@users.noreply.github.com>
2021-12-27 21:44:52 +0100

    dsf

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ test_df = pd.read_csv('./RepML/data/Splitted_full/Hyperion_test.csv', na_filter=
 
 model_name = "m-polignano-uniba/bert_uncased_L-12_H-768_A-12_italian_alb3rt0"
 
-train_dataset, val_dataset = train_val_split(df, model_name, subsample=False)
+train_dataset, val_dataset = train_val_split(df, model_name, subsample=True)
 test_dataset = HyperionDataset(test_df, model_name)
 
 class_weights = [107.0, 11.88888888888889, 1.2829003711104767, 8.794520547945206, 17.976,
@@ -38,7 +38,7 @@ class_weights = [107.0, 11.88888888888889, 1.2829003711104767, 8.794520547945206
     2.036248300860897, 12.803418803418804,7.953982300884956, 4.832258064516129, 4.192164179104478,
     9.115618661257606, 2.9295958279009127, 4.816720257234727, 21.921951219512195, 5.1068181818181815,
     1.0, 4.026881720430108, 2.646643109540636]
-trainer = MPTrainer(batch_size, learning_rate, n_epochs, torch.nn.NLLLoss(weight = torch.HalfTensor(class_weights)))
+trainer = MPTrainer(batch_size, learning_rate, n_epochs, torch.nn.NLLLoss(weight = torch.Tensor(class_weights)))
 
 
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=23)
