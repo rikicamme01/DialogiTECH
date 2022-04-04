f2b0919774b880bf8c01e9c383c2094571c86387
Michele Bortone <33963215+mibo8@users.noreply.github.com>
2022-04-04 12:52:00 +0200

    repo path from cmd

--- src/scripts/bert_cls_train.py
+++ src/scripts/bert_cls_train.py
@@ -15,25 +15,26 @@ from utils.utils import seed_everything
 from loggers.neptune_logger import NeptuneLogger
 from utils.utils import plot_confusion_matrix, plot_f1, plot_loss
 
+if len(sys.argv) != 2:
+    print("ERROR:  config_file path not provided")
+    sys.exit(1)
 
+# Repository paths
+# './' local
+# './RepML/ cluster
 
 try: 
-    with open ('./config/bert_cls_train.yml', 'r') as file:
-        config = yaml.safe_load(file)
+    with open (sys.argv[1] + 'config/bert_cls_train.yml', 'r') as file:
+        config = yaml.safe_load(file)        
 except Exception as e:
     print('Error reading the config file')
+    sys.exit(1)
+print('config file loaded!')
 
 seed_everything(config['seed'])
 
-if config['mode'] == 'cluster':
-    df = pd.read_csv('./RepML/data/processed/Splitted_full/Hyperion_train.csv', na_filter=False)
-    test_df = pd.read_csv('./RepML/data/processed/Splitted_full/Hyperion_test.csv', na_filter=False)
-elif config['mode'] == 'sagemaker':
-    df = pd.read_csv('../data/processed/Splitted_full/Hyperion_train.csv', na_filter=False)
-    test_df = pd.read_csv('../data/processed/Splitted_full/Hyperion_test.csv', na_filter=False)
-else:
-    df = pd.read_csv('./data/processed/Splitted_full/Hyperion_train.csv', na_filter=False)
-    test_df = pd.read_csv('./data/processed/Splitted_full/Hyperion_test.csv', na_filter=False)
+df = pd.read_csv(sys.argv[1] + 'data/processed/Splitted_full/Hyperion_train.csv', na_filter=False)
+test_df = pd.read_csv(sys.argv[1] + 'data/processed/Splitted_full/Hyperion_test.csv', na_filter=False)
 
 logger = NeptuneLogger()
 logger.run['config'] = config
