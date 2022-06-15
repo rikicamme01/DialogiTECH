4860d1164c2d866fcd051c015067de3d588eff0e
mibo8 <bortone.michele@gmail.com>
2022-06-15 19:51:55 +0200

    cls_last hidden state

--- src/models/bert_rep.py
+++ src/models/bert_rep.py
@@ -140,8 +140,11 @@ class BertRep():
         with torch.no_grad():                          
             outputs = self.model(input_ids, attention_mask, output_hidden_states= True)
         hs = outputs['hidden_states'][-1].cpu()
-        hs = hs[:][0] ## [CLS] hidden states
+        print(hs.shape)
+        hs = hs[:,0,:] ## [CLS] hidden states
+        print(hs.shape)
         hs = torch.mean(hs, 0) ## spans average
+        print(hs.shape)
         return hs.tolist()
     
 
